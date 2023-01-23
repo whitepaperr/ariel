c64c0d230965dd59f1fd91b977bec7da817d39ae
haeun <haeuni437@gmail.com>
2023-01-22 20:20:52 -0800

    portfolio

---
+++ foundations-mastery-protests/README 복사본.md
@@ -0,0 +1,96 @@
+# Assignment 1: Protests
+The past few years in the United States, there has been a surge in protests in support of Black Lives Matter, gender equity, and other social issues. In this assignment, you'll work with data from [CountLove](https://countlove.org/) -- the same data often [cited](https://www.nytimes.com/2020/08/28/us/black-lives-matter-protest.html) by the New York Times -- to learn more about demonstrations over the past few years. 
+
+By completing the assignment, you will demonstrate the following skills:
+
+- Use of **version control** for managing your code
+- Declaring document rendering using **markdown** syntax
+- Foundational programming skills in R. 
+
+
+## Background Research
+Before diving into this (or any) dataset, it's important to have _domain familiarity_ (i.e., to know something about the topic). As preparation, I'm asking that you read **three articles** about protests in the U.S., and provide a brief 1 - 2 sentence summary or takeaway from each one. 
+
+In the section below, create an **unordered list** of the three articles you found. Make sure to provide an appropriate markdown link (_not_ just the URL) to the article in addition to your 1 - 2 sentence summary. 
+
+- https://www.columbian.com/news/2020/jul/22/driver-accused-of-hitting-2-protesters-pleads-not-guilty/
+
+A man accused of driving a car on to a closed Seattle freeway this month and hitting two protesters, **killing** one and seriously injuring the other, pleaded not guilty. Seattle has been the site of prolonged unrest following the May police killing of George Floyd in Minneapolis, which sparked nationwide protests.
+
+- https://www.seattletimes.com/seattle-news/seattle-police-arrests-16-at-capitol-hill-protest-on-saturday/
+
+Sixteen protesters were arrested on Capitol Hill on Saturday night after some in the group shattered windows at a Starbucks on East Olive Way and threw what appeared to be fireworks into the store.
+
+- https://komonews.com/news/local/woman-says-she-shot-man-at-uw-protest-to-prevent-stabbing
+
+A woman who **shot** a man during protests of a right-wing speaker at the University of Washington says she fired to stop her husband from getting stabbed.
+
+## Accompanying Image 
+In this section, please **display one image** to accompany your text, and describe _why_ you included it (~2 - 3 sentences). This will require that you download an image into your project folder. In your description, use **bold** and _italics_ (at least once, for practice) to emphasize some of your points. 
+
+![protestor in Seattle](Racial_Injustice_Seattle_15944.jpg-2d0c5.jpg)
+
+I chose this picture because I felt so **bad** for the woman protesting. Moreover, It was scary to see the police to see the police _indiscriminately_ killing a lot of people.
+
+## Analysis
+At this point, you should open up your `analysis.R` script to begin working with the data. The script will guide you through an initial analysis of the data. Throughout the script, there are prompts labeled **Reflection**. Please write 1 - 2 sentences for each of these reflections below:
+
+- What does the difference between the mean and the median tell you about the *distribution* of the data?
+
+The difference between the mean and the median is big, which means that the distribution of the data is wide.
+
+- Does the number of protests in Washington surprise you? Why or why not?
+
+It made me surprised because all people looked aggressive and killed one another. 
+
+- Looking at the `state_table` variable, what data quality issues do you notice, and how would you use that to change your analysis (no need to actually change your analysis)?
+
+I can notice the state name and the frequency of them, and I think there is no changeg in my analysis after I read this variable.
+
+- Does the change in the number of protests from 2019 to 2020 surprise you? Why or why not?
+
+I did not surprised because the ratio does not have a big difference as I expected.
+
+- Do a bit of research. Find at least *two specific policies* that have been changed as a result of protests in 2020. These may be at the city, state, or University level. Please provide a basic summary, as well as a link to each article.
+
+https://www.vox.com/2020/6/10/21283966/protests-george-floyd-police-reform-policy
+Improving police accountablitiy and restricting use of force/ Tear gas bans/ Defunding the police and reducing police presence/ Tearing down confederate statues and racist symbols
+
+https://www.businessinsider.com/13-concrete-changes-sparked-by-george-floyd-protests-so-far-2020-6
+Editors have resigned, statues have toppled, and police reforms have been announced in several cities across the US.
+
+- Take a look (`View()`) your `high_level_table` variable. What picture does this paint of the U.S.?
+
+There are lots of protest related to civil rights and imigration.
+
+## Critical Reflection
+Now that you have had time to work with the data and visualize it, I want you to practice thinking critically about the dataset we provided. An important part of data analysis is reflecting on the assumptions, limitations, and gaps in your datasets. Remember that all datasets have to make assumptions and not all assumptions are bad or unreasonable! Practicing these skill will help you in your career to be self critical and ethically alert.
+
+For this section, please write 2-3 sentences for each of the reflections below:
+
+- How was the dataset collected and who collected the data? 
+
+When people use the internet and record their knowledge, dataset can be collected. Survey, interview, or some articles and writings can be a source of collecting data. I believe that people have always been collecting data in our daily lives.
+
+- What assumptions does the dataset make? List at least two assumptions you identified. For inspiration, check out this [blog post](https://towardsdatascience.com/check-your-assumptions-about-your-data-20be250c143) that describes one method for identifying data assumptions.   
+
+By making assumptions with dataset, we can predict the future and prepare for it such as weather forecast. After I analysis the dataset, I can assume there can be lots of protests in the future as well as people need to be aware of some violence and pursue peace protests.
+
+- What data is missing from the dataset? Think about if there are any data points you would have liked to have seen in the dataset. 
+
+Protests in the past have been mostly violent. However, there may have been nonviolent protests in recent times. I thought it would have been nice to indicate whether the protests were violent or not.
+
+## Final Thoughts
+When you are finished, with your analysis, please answer the following questions in 1-2 sentences each. 
+
+- What about the analysis surprised you?
+
+It was surprising that complex data could be sorted out at once with just a few codes and functions. 
+
+- What parts of this analysis did you find challenging?
+
+Making function is the hardest part. Unlike using a function that has been created, it was too complicated to create your own code.
+
+- What types of analysis do you wish you were able to do with the dataset, but currently don't have the technical skills to do?
+
+I am not sure whether technical skills already exist or not, but I would like to distinguish the sources and categorize them.
---
+++ foundations-mastery-protests/analysis 복사본.R
@@ -0,0 +1,217 @@
+# Overview ----------------------------------------------------------------
+
+# Assignment 1: Analysis of the protest data from Crowd Love
+# For each question/prompt, write the necessary code to calculate the answer.
+# For grading, it's important that you store your answers in the variable names
+# listed with each question in `backtics`.
+# For each prompt marked `Reflection`, please write a response
+# in your `README.md` file.
+
+# Part 1:Set up -----------------------------------------------------------
+
+# In this section, you're loading the data and necessary packages.
+# Load the `stringr` package, which you'll use later.
+library(stringr)
+# Load the data from https://countlove.org/data/data.csv
+# into a variable called `protests`
+protests <- read.csv("https://countlove.org/data/data.csv")
+# How many protests are in the dataset? `num_protests`
+num_protests <- nrow(protests)
+# How much information is available about each protest? `num_features`
+num_features <- ncol(protests)
+
+# Part 2: Attendees -------------------------------------------------------
+
+# In this section, you're exploring the number of attendees.
+
+# Extract the `Attendees` column into a variable called `num_attendees`
+num_attendees <-protests$Attendees
+# What is the lowest number of attendees? `min_attendees`
+# (hint for this and other calculations: you'll need to consider missing values)
+min_attendees <-min(num_attendees, na.rm = TRUE)
+# What is the highest number of attendees? `max_attendees`
+max_attendees <- max(num_attendees, na.rm = TRUE)
+# What is the mean number of attendees? `mean_attendees`
+mean_attendees <- mean(num_attendees, na.rm = TRUE)
+# What is the median number of attendees? `median_attendees`
+median_attendees <- median(num_attendees, na.rm = TRUE)
+# What is the difference between the mean and median number of attendees?
+# `mean_median_diff`
+mean_median_diff <- abs(mean_attendees - median_attendees)
+# Reflection: What does the difference between the mean and the median
+# tell you about the *distribution* of the data? (if you're unfamiliar with
+# working with distibutions, feel free to ask your TA for clarification)
+
+# To further assess the distribution of values, create a boxplot of the number
+# of attendees using the `boxplot()` function.
+# Store the plot in a variable called `attendess_distribution`
+# (Note, we'll use much more refined plotting methods, and pay far
+# more attention to detail later in the course)
+attendess_distriution <- boxplot(num_attendees)
+# Create another boxplot of the *log* of the number of attendees.
+# Store the plot in a variable `log_attendees_distribution`.
+# (note, you will see a warning in the console, which is expected)
+log_attendees_distribution <- boxplot(log(num_attendees))
+
+# Part 3: Locations -------------------------------------------------------
+
+# In this section, you're exploring where protests happened.
+
+# Extract the `Location` column into a variable called `locations`
+locations <- protests$Location
+# How many *unique* locations are in the dataset? `num_locations`
+num_locations <- unique(locations)
+# How many protests occured in Washington? `num_in_wa`
+# (hint: use a function from the stringr package to detect the letters "WA")
+num_in_wa <- sum(str_detect(locations, pattern = "WA"))
+# What proportion of protests occured in Washington? `prop_in_wa`
+prop_in_wa <- num_in_wa / num_protests
+# Reflection: Does the number of protests in Washington surprise you?
+# Why or why not?
+
+# Write a function `count_in_location()` that accepts (as a parameter)
+# a `location` name, and returns the sentence (note: spacing and punctuation):
+# "There were N protests in LOCATION.", where N is the number of
+# protests that occured in that location, and LOCATION is the parameter that
+# was provided into the function.
+# Note, you should count the number of locations that *mfitatch* the parameter
+# put into the function, so `Seattle` should be a match for "Seattle, WA"
+count_in_location <- function(name){
+  num_in_name <- sum((str_detect(locations, name)))
+  sprintf("There were %s protests in %s", num_in_name, name)
+}
+count_in_location("Seattle, WA")
+# Use your function above to describe the number of protests in "Washington, DC"
+# `dc_summary`
+dc_summary <- count_in_location("Washington, DC")
+# Use your function above to describe the number of protests in "Minneapolis"
+# `minneapolis_summary`
+minneapolis_summary<- count_in_location("Minneapolis")
+# Create a new vector `states` which is the last two characters of each
+# value in the `locations` vector. Hint, you may want to again use the
+# `stringr` package
+states <- str_sub(locations, nchar(locations) -2, nchar(locations))
+# Create a vector of the unique states in your dataset. `uniq_states`
+uniq_states <- unique(states)
+# Create a summary sentence for each state by passing your `uniq_states`
+# variable and `count_in_location` variables to the `sapply()` function.
+# Store your results in `state_summary`
+# (don't miss how amazing this is! Very powerful to apply your function to an
+# entire vector *at once* with `sapply()`)
+state_summary <- sapply(uniq_states, count_in_location)
+# Create a summary table by passing your `states` variable to the `table()`
+# funciton, and storing the result in a variable `state_table`.
+state_table <- table(states)
+# Optional: use the View() function to more easily read the table
+View(state_table)
+# Reflection: Looking at the `state_table` variable, what data quality issues
+# do you notice, and how would you use that to change your analysis (no need
+# to actually change your analysis)?
+
+# What was the maximum number of protests in a state? `max_in_state`
+# (hint: use your `state_table` variable)
+max_in_state <- max(state_table)
+
+# Part 4: Dates -----------------------------------------------------------
+
+# In this section, you're exploring *when* protests happened.
+
+# Extract the `Date` column into a variable called `dates` by passing the
+# column to the `as.Date()` function (this will process the values as dates,
+# which are *luckily* already in an optimal format for parsing)
+dates <- as.Date(protests$Date)
+# What is the most recent date in the dataset? `most_recent`
+most_recent <- max(dates)
+# What is the earliest date in the dataset? `earliest`
+earliest <- min(dates)
+# What is the length of the timespan of the dataset? `time_span`
+# hint: R can do math with dates pretty well by default!
+time_span <-length(dates)
+# Create a vector of the dates that are in 2020 `in_2020`
+year <- substr(dates, 1, 4)
+in_2020 <- dates[year == 2020]
+# Create a vector of the dates that are in 2019. `in_2019`
+in_2019 <- dates[year == 2019]
+# What is the ratio of the number of protests in 2020 compared to 2019?
+# `ratio_2020_2019`
+ratio_2020_2019 <- sum(in_2020) / sum(in_2019)
+# Reflection: Does the change in the number of protests from 2019 to 2020
+# surprise you? Why or why not?
+
+# Write a function `count_on_date()` that accecpts as a parameter a `date`,
+# and returns the sentence:
+# "There were N protests on DATE.", where N is the number of protests on that
+# date, and DATE is the date provided
+count_on_date <- function(date){
+  num_in_date <- sum((str_detect(dates, date)))
+  sprintf("There were %s protests on %s", num_in_date, date)
+}
+# Using your function you just wrote, how many protests were there on
+# May 24th, 2020? `num_may_24`
+num_may_24 <- count_on_date("2020-05-24")
+# Using your function you just wrote, how many protests were there on
+# May 31th, 2020? `num_on_may_31`
+num_on_may_31 <- count_on_date("2020-05-31")
+# For more on this timeline, see:
+# https://www.nytimes.com/article/george-floyd-protests-timeline.htmls
+
+# How many protests occured each month in 2020? `by_month_table`
+# Hint: use the `months()` function, your `in_2020` dates, and the `table()`
+# Function. If you like, you can do this in multiple different steps.
+by_month_table <- table(months(in_2020))
+View(by_month_table)
+# As a comparison, let's assess the change between July 2019 and July 2020.
+# What is the *difference* in the number of protests between July 2020 and
+# July 2019? You'll want to do this in multiple steps as you see fit, though
+# your answer should be stored in the variable `change_july_protests`.
+by_month_2019 <- table(months(in_2019))
+July_2019 <- by_month_2019[6]
+July_2020 <- by_month_table[6]
+change_july_protests <- abs(July_2019 - July_2020)
+# Reflection: do a bit of research. Find at least *two specific policies* that
+# have been changed as a result of protests in 2020. These may be at the
+# city, state, or University level. Please provide a basic summary, as well as a
+# link to each article.
+
+
+# Part 5: Protest Purpose -------------------------------------------------
+
+# In this section, you're exploring *why* protests happened
+# Extract the `Event..legacy..see.tags.` column into a variable called `purpose`
+purpose <- protests$Event..legacy..see.tags.
+# How many different purposes are listed in the dataset? `num_purposes`
+num_purposes <- length(purpose)
+# That's quite a few -- if you look at -- View() -- the vector, you'll notice
+# a common pattern for each purpose. It's listed as:
+# SOME_PURPOSE (additiona_detail)
+# To get a higher level summary, create a variable `high_level_purpose` by
+# extracting *everything before the first parenthesis* in each value
+# in the vector. For example, from "Civil Rights (Black Women's March)"
+# you would extract "Civil Rights". You'll also have to *remove the space*
+# before the first parenthasis.
+# Hint: this will take a little bit of googling // trial and error. Be patient!
+high_level_purpose <- sub(" \\(.*", "", purpose)
+print(high_level_purpose)
+# How many "high level" purposes have you identified? `num_high_level`
+num_high_level <- length(unique(high_level_purpose))
+# Create a table that counts the number of protests for each high level purpose
+# `high_level_table`
+high_level_table <- high_level_purpose
+View(high_level_table)
+# Reflection: Take a look (`View()`) your `high_level_table` variable. What
+# picture does this paint of the U.S.?
+
+
+  # Part 6: Independent Exploration -----------------------------------------
+
+# As a last step, you should write your own function that allows you to
+# quickly ask questions of the dataset. For example, in the above sections,
+# you wrote functions to ask the same question about different months, or
+# locations. If you need any guidance here, feel free to ask! 
+
+my_function <- function(nnn){
+  ans <- sum((str_detect(purpose, nnn)))
+  sprintf("There were %s prtests for %s.", ans, nnn)
+}
+my_function("Healthcare")
+
