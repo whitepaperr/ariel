cc6b47d92d3fa94991a3d64530285049eaffedc9
whitepaperr <91901674+whitepaperr@users.noreply.github.com>
2023-03-08 16:48:30 -0800

    Update first_chart .R

--- info201/final project-world happiness report/first_chart .R
+++ info201/final project-world happiness report/first_chart .R
@@ -3,7 +3,7 @@ library(dplyr)
 library(tidyverse)
 library(ggplot2)
 #2019 World Happiness Dataset
-wh <- read_csv('/Users/haeun/git/INFO 201/final-project-umme-kulsum-j/data/world_happiness_report/2019.csv')
+wh <- read_csv('/Users/haeun/git/info201/final-project-umme-kulsum-j/data/world_happiness_report/2019.csv')
 View(wh)
 histogram <- ggplot(wh, aes(x=Generosity)) + 
   geom_histogram(binwidth=.1, color="black", fill="lightblue") + 
