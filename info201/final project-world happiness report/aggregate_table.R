025e2e7c00ef03241eafb9a544f24697b305fe30
whitepaperr <91901674+whitepaperr@users.noreply.github.com>
2023-03-08 16:48:10 -0800

    Update aggregate_table.R

--- info201/final project-world happiness report/aggregate_table.R
+++ info201/final project-world happiness report/aggregate_table.R
@@ -2,7 +2,7 @@ library(tidyverse)
 library(data.table)
 
 #load data
-data2015 <- read.csv("/Users/haeun/git/INFO 201/final-project-umme-kulsum-j/data/world_happiness_report/2015.csv")
+data2015 <- read.csv("/Users/haeun/git/info201/final-project-umme-kulsum-j/data/world_happiness_report/2015.csv")
 
 #group by region
 data2015 <- data2015 %>%
